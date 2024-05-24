4ea3f43563dbde3fa366cb5dcee5b0fd69a3bf73
GBRP-99 <gabrielperespoa@gmail.com>
2024-05-23 21:26:32 -0300

    Update

--- Mundo 01/ex019.py
+++ Mundo 01/ex019.py
@@ -3,5 +3,6 @@ a1 = str(input("O 1° aluno é:"))
 a2 = str(input("O 2° aluno é:"))
 a3 = str(input("O 3° aluno é:"))
 a4 = str(input("O 4° aluno é:"))
-lista = [a3, a4, a2, a1]
-print(f"Quem deve apagar o quadro é {choice(lista)}")
+# o sorteio é uma lista que entre [] contem as variaveis coletadas em formato str
+sorteio = [a3, a4, a2, a1]
+print(f"Quem deve apagar o quadro é {choice(sorteio)}")
