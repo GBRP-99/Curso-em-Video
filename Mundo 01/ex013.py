0a76d84fd48b7c58a9443b8d3c73c323d5e93a2c
GBRP-99 <gabrielperespoa@gmail.com>
2024-05-23 21:17:47 -0300

    Update

--- Mundo 01/ex013.py
+++ Mundo 01/ex013.py
@@ -1,2 +1,3 @@
 sal = float(input("O salário do funcionário é de R$"))
-print(f"Com aumento de 15% ele passa a receber {sal+(sal*15/100):.2f}")
+aum = float(input("QUal a porcentagem (%) do aumento?"))
+print(f"Com aumento de 15% ele passa a receber {sal+(sal*aum/100):.2f}")
