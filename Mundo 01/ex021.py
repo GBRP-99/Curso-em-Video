b82e52f33db6e6fb6f4deb8026ed62ff73a4e728
GBRP-99 <gabrielperespoa@gmail.com>
2024-05-24 14:48:31 -0300

    Update

--- Mundo 01/ex021.py
+++ Mundo 01/ex021.py
@@ -2,7 +2,7 @@
 
 import pygame
 pygame.init()
-pygame.mixer.music.load('audio.mp3')
+pygame.mixer.music.load('all_workout.mp3.mp3')
 # Lil Wayne - All Workout
 pygame.mixer_music.play()
 while pygame.mixer.music.get_busy():
