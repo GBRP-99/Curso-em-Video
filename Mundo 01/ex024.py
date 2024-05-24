e55efe4a5f785f8e93661de0860c6b628dc9f1bd
GBRP-99 <gabrielperespoa@gmail.com>
2024-05-24 17:52:33 -0300

    Update

--- Mundo 01/ex024.py
+++ Mundo 01/ex024.py
@@ -1,3 +1,4 @@
 # Crie um programa que leia o nome de uma cidade diga se ela começa ou não com o nome “SANTO”.
-cidade = str(input("Digite o nome da cidade: ")).strip()
-print(f"A cidade possui o nome 'SANTO'? {cidade[:5].upper() == 'Santo'}")
+
+cidade = str(input("Digite o nome da cidade: ")).strip().upper()
+print(f"A cidade começa com o nome 'Santo'? {cidade[:5] == 'SANTO'}")
