7d17514b9a3c95308fa02597a8904a8a2ba042cd
GBRP-99 <gabrielperespoa@gmail.com>
2024-05-24 16:20:54 -0300

    Update

---
+++ Mundo 01/ex022.py
@@ -0,0 +1,10 @@
+# Crie um programa que leia o nome completo de uma pessoa e mostre:
+# O nome com todas as letras maiúsculas e minúsculas.
+# Quantas letras ao total (sem considerar espaços).
+# Quantas letras tem o primeiro nome.
+
+nome = str(input("Digite seu nome completo:"))
+print(f"Seu nome em maiusculas é {nome.upper()}.")
+print(f"Seu nome em minusculas é {nome.lower()}.")
+print(f"Seu nome completo possui {len(nome.replace(" ", ""))} letras.")
+print(f"Seu primeiro nome possui {len(nome.split()[0])} letras")
