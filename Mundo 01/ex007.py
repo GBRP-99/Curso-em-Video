a07eb23072e4e74f8d9dac385073b21fae7fc006
GBRP-99 <gabrielperespoa@gmail.com>
2024-05-23 21:12:34 -0300

    Update

--- Mundo 01/ex007.py
+++ Mundo 01/ex007.py
@@ -2,6 +2,7 @@ nome = input("O nome do aluno é: ")
 n1 = float(input("Digite a 1° nota: "))
 n2 = float(input("Digite a 2° nota: "))
 print(f"{nome} tirou as notas {n1:.1f} e {n2:.1f}, a média foi de {(n1+n2)/2:.1f}")
+# o if é opicional
 if ((n1+n2)/2) >= 6:
     print("Está arpovado")
 else:
