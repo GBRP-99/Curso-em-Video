071ee03c8323518e63b4383066bb52b6b4c6ea98
GBRP-99 <gabrielperespoa@gmail.com>
2024-05-24 23:21:40 -0300

    Update

--- Exercicios/ex027.py
+++ Exercicios/ex027.py
@@ -1,6 +1,6 @@
 # Faça um programa que leia o nome completo de uma pessoa,
 # mostrando em seguida o primeiro e o último nome separadamente.
 
-nome = str(input("Digite seu nome completo: ")).strip().title()
-print(f"Seu primeiro nome é {nome.split()[0]}.")
-print(f"E seu ultimo nome é {nome.split()[-1]}.")
+nome = str(input("Digite seu nome completo: ")).strip().lower()
+print(f"Seu primeiro nome é {nome.split()[0].title()}.")
+print(f"E seu ultimo nome é {nome.split()[-1].title()}.")
