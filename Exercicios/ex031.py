16a0b1bf76ef7e7f4cf00d18e35fd5741cef3f67
GBRP-99 <gabrielperespoa@gmail.com>
2024-05-25 16:14:13 -0300

    Update

---
+++ Exercicios/ex031.py
@@ -0,0 +1,9 @@
+# Desenvolva um programa que pergunte a distância de uma viagem em Km.
+# Calcule o preço da passagem, cobrando R$0,50 por Km para viagens de até 200Km
+# e R$0,45 parta viagens mais longas.
+
+d = int(input("Quantos km/s você irá rodar? "))
+if d <= 200:
+    print(f"Para está viagem a passagem será de R${(d*0.50):.2f}")
+else:
+    print(f"Para está viagem a passagem será de R${(d*0.45):.2f}")
