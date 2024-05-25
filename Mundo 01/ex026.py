6cabb5bbd8126bdecccb207e37943d57e1f52e9f
GBRP-99 <gabrielperespoa@gmail.com>
2024-05-24 22:44:19 -0300

    Update

--- Mundo 01/ex026.py
+++ Mundo 01/ex026.py
@@ -1,2 +1,7 @@
 # Faça um programa que leia uma frase pelo teclado e mostre quantas vezes aparece a letra “A”,
 # em que posição ela aparece a primeira vez e em que posição ela aparece a última vez.
+
+msg = str(input("Digite uma frase: ")).strip().upper()
+print(f"A letra 'A' se repete {msg.count('A')}x")
+print(f"Sua primeira aparição é {msg.find('A')}")
+print(f"E a sua ultima aparição é {msg.rfind('A')}")
