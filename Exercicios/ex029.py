b1f49d400b646ce4cb0aa4d602ff7e648ef54962
GBRP-99 <gabrielperespoa@gmail.com>
2024-05-25 15:48:36 -0300

    Update

---
+++ Exercicios/ex029.py
@@ -0,0 +1,11 @@
+# Escreva um programa que leia a velocidade de um carro.
+# Se ele ultrapassar 80Km/h, mostre uma mensagem dizendo que ele foi multado.
+# A multa vai custar R$7,00 por cada Km acima do limite.
+
+velo = int(input("A que velocidade(km/h) estava o veiculo? "))
+acima = velo - 80
+if velo > 80:
+    print(f"Veiculo estava a {acima}km/r acima do limite!")
+    print(f"Receberá a multa de R${acima*7}")
+else:
+    print("Velocidade permitida, não será penalizado.")
