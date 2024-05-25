fbd4e9154027af7c66b2cb588fd243d9354ca427
GBRP-99 <gabrielperespoa@gmail.com>
2024-05-25 16:16:39 -0300

    Update

--- Exercicios/ex031.py
+++ Exercicios/ex031.py
@@ -2,7 +2,7 @@
 # Calcule o preço da passagem, cobrando R$0,50 por Km para viagens de até 200Km
 # e R$0,45 parta viagens mais longas.
 
-d = int(input("Quantos km/s você irá rodar? "))
+d = float(input("Quantos km/s você irá rodar? "))
 if d <= 200:
     print(f"Para está viagem a passagem será de R${(d*0.50):.2f}")
 else:
